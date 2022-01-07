264e2a8d5822307705a51504213930c9601c117e
Julian Sarmiento <juliansarmiento10@outlook.com>
2022-01-07 01:51:49 -0500

    parte de los estilos

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,11 +7,11 @@ const api = {
 const Search = () => {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
+        .then((res) => res.json())
+        .then((result) => {
           setWeather(result);
           setQuery("");
           console.log(result);
@@ -19,17 +19,46 @@ const Search = () => {
     }
   };
   return (
-    <div className={styles.container}>
-      <div className={styles.searchbox}>
-        <input
-          type="text"
-          className={styles.searchbar}
-          placeholder="Input city for search..."
-          onChange={(e) => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        />
-      </div>
+    <div className={(typeof weather.main != 'undefined') ? (weather.weather[0].main === "Rain") ? `${styles.rain}` : `${styles.clear}` : 
+    `${styles.clear}` }>
+      <main>
+        <div className={styles.container}>
+          <div className={styles.searchbox}>
+            <input
+              type="text"
+              className={styles.searchbar}
+              placeholder="Input city for search..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          <div>
+            {typeof weather.main != "undefined" ? (
+              <div className={styles.container_lctbx}>
+                <div className={styles.location_box}>
+                  <div className={styles.location}>
+                    {weather.name}, {weather.sys.country}
+                  </div>
+                  <div className={styles.date}>
+                    {new Date(
+                      weather.dt * 1000 + weather.timezone * 1000
+                    ).toUTCString()}
+                  </div>
+                </div>
+                <div className={styles.weather_box}>
+                  <div className={styles.temp}>{weather.main.temp}°C</div>
+                  <div className={styles.weather}>
+                    {weather.weather[0].main}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
